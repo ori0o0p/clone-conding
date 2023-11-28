38626ffbd11fd0c8d65c471e00c169a8183d8366
101 <tmddnjs001133@dsm.hs.kr>
2023-11-28 19:35:40 +0900

    이메일 어노테이션 추가

--- src/main/java/com/example/instagramclonecoding/domain/auth/dto/request/LoginRequest.java
+++ src/main/java/com/example/instagramclonecoding/domain/auth/dto/request/LoginRequest.java
@@ -1,4 +1,6 @@
 package com.example.instagramclonecoding.domain.auth.dto.request;
 
-public record LoginRequest(String email, String password) {
+import jakarta.validation.constraints.Email;
+
+public record LoginRequest(@Email String email, String password) {
 }
