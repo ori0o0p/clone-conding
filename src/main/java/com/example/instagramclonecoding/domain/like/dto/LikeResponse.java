4a06692a92ad5a8aaac35ab38093cdb795baa073
101 <tmddnjs001133@dsm.hs.kr>
2023-11-30 17:20:10 +0900

    수정

--- src/main/java/com/example/instagramclonecoding/domain/like/dto/LikeResponse.java
+++ src/main/java/com/example/instagramclonecoding/domain/like/dto/LikeResponse.java
@@ -1,18 +1,15 @@
 package com.example.instagramclonecoding.domain.like.dto;
 
-import com.example.instagramclonecoding.domain.article.entity.Article;
 import com.example.instagramclonecoding.domain.like.entity.Like;
 import com.example.instagramclonecoding.domain.user.entity.User;
 import lombok.Builder;
 
 @Builder
 public record LikeResponse(
-        User user,
-        Article article
+        User user
+//        Article article
 ) {
     public LikeResponse(Like like) {
-        this(like.getUser(), like.getArticle());
+        this(like.getUser());
     }
-
-
 }
