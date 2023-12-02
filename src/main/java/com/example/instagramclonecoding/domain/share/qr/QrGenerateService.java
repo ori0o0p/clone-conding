7bbb5aa6c18c8d631f753e8b9d9661bba463f5da
101 <tmddnjs001133@dsm.hs.kr>
2023-12-02 12:30:07 +0900

    qr 생성

---
+++ src/main/java/com/example/instagramclonecoding/domain/share/qr/QrGenerateService.java
@@ -0,0 +1,31 @@
+package com.example.instagramclonecoding.domain.share.qr;
+
+import org.springframework.stereotype.Service;
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.WriterException;
+import com.google.zxing.client.j2se.MatrixToImageWriter;
+import com.google.zxing.common.BitMatrix;
+import com.google.zxing.qrcode.QRCodeWriter;
+
+import java.awt.image.BufferedImage;
+
+@Service
+public class QrGenerateService {
+
+    public BufferedImage execute(String url) throws WriterException {
+
+        if (url.isBlank()) {
+            throw new RuntimeException("url이 비어있어요");
+        }
+        QRCodeWriter qrCodeWriter = new QRCodeWriter();
+
+        BitMatrix bitMatrix = qrCodeWriter.encode(
+                url,
+                BarcodeFormat.QR_CODE,
+                200, 200
+        );
+
+        return MatrixToImageWriter.toBufferedImage(bitMatrix);
+    }
+
+}
