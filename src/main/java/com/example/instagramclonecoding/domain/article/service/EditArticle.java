42ced33297e37405af3cb65095fcef03069920ee
101 <tmddnjs001133@dsm.hs.kr>
2023-12-04 09:24:30 +0900

    게시물 수정

---
+++ src/main/java/com/example/instagramclonecoding/domain/article/service/EditArticle.java
@@ -0,0 +1,36 @@
+package com.example.instagramclonecoding.domain.article.service;
+
+import com.example.instagramclonecoding.domain.article.dto.EditArticleRequest;
+import com.example.instagramclonecoding.domain.article.repository.ArticleRepository;
+import com.example.instagramclonecoding.domain.user.entity.User;
+import com.example.instagramclonecoding.domain.user.service.facade.UserFacade;
+import org.springframework.stereotype.Service;
+import reactor.core.publisher.Mono;
+
+import java.util.Objects;
+
+@Service
+public class EditArticle {
+    private final ArticleRepository articleRepository;
+    private final UserFacade userFacade;
+
+    public EditArticle(ArticleRepository articleRepository, UserFacade userFacade) {
+        this.articleRepository = articleRepository;
+        this.userFacade = userFacade;
+    }
+
+    // req : 게시물 ID, 수정 사항 (content)
+
+    public Mono<Void> execute(EditArticleRequest request) {
+        User user = userFacade.getUser();
+        return articleRepository.findById(request.id())
+                .switchIfEmpty(Mono.error(() -> new RuntimeException("게시물을 찾지 못함")))
+                .filter(article -> article.getWriter().equals(user))
+                .flatMap(article -> {
+                    // 수정 후 저장
+                    article.editContent(request.content());
+                    return articleRepository.save(article);
+                }).then();
+    }
+
+}
