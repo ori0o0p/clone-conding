68a0cf04c8c5d8afe98fb5e5cb0c023f330be14f
101 <tmddnjs001133@dsm.hs.kr>
2023-12-04 17:38:13 +0900

    댓글 생성

--- src/main/java/com/example/instagramclonecoding/domain/comment/service/CreateComment.java
+++ src/main/java/com/example/instagramclonecoding/domain/comment/service/CreateComment.java
@@ -1,10 +1,15 @@
 package com.example.instagramclonecoding.domain.comment.service;
 
 import com.example.instagramclonecoding.domain.article.repository.ArticleRepository;
+import com.example.instagramclonecoding.domain.comment.dto.CommentRequest;
+import com.example.instagramclonecoding.domain.comment.entity.Comment;
 import com.example.instagramclonecoding.domain.comment.repository.CommentRepository;
+import com.example.instagramclonecoding.domain.user.entity.User;
 import com.example.instagramclonecoding.domain.user.service.facade.UserFacade;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Mono;
+
+import java.util.Date;
 
 @Service
 public class CreateComment {
@@ -19,11 +24,23 @@ public class CreateComment {
     }
 
     // req : 게시물 ID, 댓글 내용(content)
-    public Mono<Void> execute() {
+    public Mono<Void> execute(CommentRequest request) {
         // 유저 정보 가져오기
+        User user = userFacade.getUser();
         // 게시물 검증
-        // 댓글 추가
+        return articleRepository.findById(request.articleId())
+                .switchIfEmpty(Mono.error(() -> new RuntimeException("게시물을 찾지 못함")))
+                .flatMap(article -> {
+                    Comment comment = Comment.builder()
+                            .content(request.content())
+                            .createdAt(new Date())
+                            .article(article)
+                            .build();
+                    // 댓글 추가 article save, comment save
+                    article.getCommentList().add(comment);
+                    return Mono.zip(commentRepository.save(comment),
+                            articleRepository.save(article));
+                })/*.onErrorResume(Mono::error)*/.then();
     }
-
 
 }
