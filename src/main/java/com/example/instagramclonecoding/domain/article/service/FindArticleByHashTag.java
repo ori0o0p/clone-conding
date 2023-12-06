9cb16a4a0d56797acd4a22411cb3dc1e5025550a
101 <tmddnjs001133@dsm.hs.kr>
2023-12-06 10:50:25 +0900

    해쉬태그로 게시물 찾기

---
+++ src/main/java/com/example/instagramclonecoding/domain/article/service/FindArticleByHashTag.java
@@ -0,0 +1,31 @@
+package com.example.instagramclonecoding.domain.article.service;
+
+import com.example.instagramclonecoding.domain.article.dto.SearchArticleResponse;
+import com.example.instagramclonecoding.domain.article.entity.Article;
+import com.example.instagramclonecoding.domain.hashTag.entity.HashTag;
+import com.example.instagramclonecoding.domain.hashTag.repository.HashTagRepository;
+import org.springframework.stereotype.Service;
+import reactor.core.publisher.Flux;
+import reactor.core.scheduler.Schedulers;
+
+@Service
+public class FindArticleByHashTag {
+    private final HashTagRepository hashTagRepository;
+
+    public FindArticleByHashTag(HashTagRepository hashTagRepository) {
+        this.hashTagRepository = hashTagRepository;
+    }
+
+    public Flux<SearchArticleResponse> execute(String hashTag) {
+        Flux<SearchArticleResponse> list = hashTagRepository.findById(hashTag)
+                .map(HashTag::getArticleList)
+                .flatMap(articles -> Flux.fromIterable(articles)
+                        .map(SearchArticleResponse::new)
+                        .collectList())
+                .flatMapMany(Flux::fromIterable)
+                .subscribeOn(Schedulers.boundedElastic());
+
+        return Flux.from(list);
+    }
+
+}
