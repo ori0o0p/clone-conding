6f140edf014501affee02cb0004ec0bdd29ecbc0
101 <tmddnjs001133@dsm.hs.kr>
2023-12-06 10:50:09 +0900

    find all

--- src/main/java/com/example/instagramclonecoding/domain/hashTag/repository/HashTagRepository.java
+++ src/main/java/com/example/instagramclonecoding/domain/hashTag/repository/HashTagRepository.java
@@ -1,7 +1,14 @@
 package com.example.instagramclonecoding.domain.hashTag.repository;
 
 import com.example.instagramclonecoding.domain.hashTag.entity.HashTag;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
+import reactor.core.publisher.Flux;
 
 public interface HashTagRepository extends ReactiveMongoRepository<HashTag, String> {
+
+    @Override
+    @Query(sort = "{tag : -1}")
+    Flux<HashTag> findAll();
+
 }
