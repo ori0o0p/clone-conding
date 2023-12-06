732e01cd910529f0d2435afef3f9dba406542116
101 <tmddnjs001133@dsm.hs.kr>
2023-12-06 11:10:19 +0900

    해시 태그 전체 조회

---
+++ src/main/java/com/example/instagramclonecoding/domain/hashTag/service/GetAllHashTag.java
@@ -0,0 +1,29 @@
+package com.example.instagramclonecoding.domain.hashTag.service;
+
+import com.example.instagramclonecoding.domain.hashTag.dto.HashTagResponse;
+import com.example.instagramclonecoding.domain.hashTag.repository.HashTagRepository;
+import org.springframework.stereotype.Service;
+import reactor.core.publisher.Flux;
+import reactor.core.scheduler.Schedulers;
+
+@Service
+public class GetAllHashTag {
+    private final HashTagRepository hashTagRepository;
+
+    public GetAllHashTag(HashTagRepository hashTagRepository) {
+        this.hashTagRepository = hashTagRepository;
+    }
+
+    public Flux<HashTagResponse> execute() {
+        Flux<HashTagResponse> list = hashTagRepository.findAll()
+                .collectList()
+                .flatMap(hashTags -> Flux.fromIterable(hashTags)
+                        .map(HashTagResponse::new)
+                        .collectList())
+                .flatMapMany(Flux::fromIterable)
+                .subscribeOn(Schedulers.boundedElastic());
+
+        return Flux.from(list);
+    }
+
+}
