ea4e4a5f933d42049ba3961a697e38f8e5f26fce
101 <tmddnjs001133@dsm.hs.kr>
2023-12-09 12:22:02 +0900

    수정

--- src/main/java/com/example/instagramclonecoding/domain/user/service/FindArticlesByUser.java
+++ src/main/java/com/example/instagramclonecoding/domain/user/service/FindArticlesByUser.java
@@ -1,11 +1,10 @@
 package com.example.instagramclonecoding.domain.user.service;
 
-import com.example.instagramclonecoding.domain.article.dto.MyArticleResponse;
+import com.example.instagramclonecoding.domain.article.dto.ArticleResponse;
 import com.example.instagramclonecoding.domain.article.entity.Article;
 import com.example.instagramclonecoding.domain.article.repository.ArticleRepository;
 import com.example.instagramclonecoding.domain.comment.collection.CommentCollection;
 import com.example.instagramclonecoding.domain.like.collection.LikeCollection;
-import com.example.instagramclonecoding.domain.user.entity.User;
 import com.example.instagramclonecoding.domain.user.service.facade.UserFacade;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Flux;
@@ -13,31 +12,32 @@ import reactor.core.publisher.Mono;
 import reactor.core.scheduler.Schedulers;
 
 @Service
-public class FindMyArticles {
+public class FindArticlesByUser {
     private final ArticleRepository articleRepository;
     private final UserFacade userFacade;
 
-    public FindMyArticles(ArticleRepository articleRepository, UserFacade userFacade) {
+    public FindArticlesByUser(ArticleRepository articleRepository, UserFacade userFacade) {
         this.articleRepository = articleRepository;
         this.userFacade = userFacade;
     }
 
-    public Flux<MyArticleResponse> execute() {
-        User user = userFacade.getUser();
+    public Flux<ArticleResponse> execute(String userId) {
+        //User user = userFacade.getUser();
 
-        Flux<MyArticleResponse> list = articleRepository.findByUserId(user.getId())
+        Flux<ArticleResponse> list = articleRepository.findByUserId(userId)
                 .flatMap(this::rapping)
                 .subscribeOn(Schedulers.boundedElastic());
 
         return Flux.from(list);
     }
 
-    private Mono<MyArticleResponse> rapping(Article article) {
+    private Mono<ArticleResponse> rapping(Article article) {
         LikeCollection likeCollection = new LikeCollection(article.getLikeList());
         CommentCollection commentCollection = new CommentCollection(article.getCommentList());
 
-        return Mono.just(MyArticleResponse.builder()
+        return Mono.just(ArticleResponse.builder()
                         .id(article.getId())
+                        .writer(article.getWriter())
                         .imageURL(article.getImageURL())
                         .content(article.getContent())
                         .createdAt(article.getCreatedAt())
