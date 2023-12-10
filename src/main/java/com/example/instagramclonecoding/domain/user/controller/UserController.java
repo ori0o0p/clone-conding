048bf6b70fe078c9533e0a144edfc0b1d825bc89
101 <tmddnjs001133@dsm.hs.kr>
2023-12-10 21:32:58 +0900

    컨트롤러 설정

--- src/main/java/com/example/instagramclonecoding/domain/user/controller/UserController.java
+++ src/main/java/com/example/instagramclonecoding/domain/user/controller/UserController.java
@@ -1,13 +1,11 @@
 package com.example.instagramclonecoding.domain.user.controller;
 
 import com.example.instagramclonecoding.domain.article.dto.ArticleResponse;
+import com.example.instagramclonecoding.domain.user.dto.LastArticleIdRequest;
 import com.example.instagramclonecoding.domain.user.entity.User;
 import com.example.instagramclonecoding.domain.user.service.FindArticlesByUser;
 import com.example.instagramclonecoding.domain.user.service.facade.UserFacade;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Flux;
 
 @RestController
@@ -22,14 +20,15 @@ public class UserController {
     }
 
     @GetMapping("/articles/me")
-    public Flux<ArticleResponse> getMyArticles() {
+    public Flux<ArticleResponse> getMyArticles(@RequestBody LastArticleIdRequest request) {
         User user = userFacade.getUser();
-        return findArticlesByUser.execute(user.getId());
+        return findArticlesByUser.execute(user.getId(), request.lastArticleId(), 18);
     }
 
     @GetMapping("/articles/{userId}")
-    public Flux<ArticleResponse> getOtherUsersArticles(@PathVariable String userId) {
-        return findArticlesByUser.execute(userId);
+    public Flux<ArticleResponse> getOtherUsersArticles(@PathVariable String userId,
+                                                       @RequestBody LastArticleIdRequest request) {
+        return findArticlesByUser.execute(userId, request.lastArticleId(), 18);
     }
 
 }
