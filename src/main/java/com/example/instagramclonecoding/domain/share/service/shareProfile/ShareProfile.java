6c9ccd4ed4772ccf195697178ad7b0ba169ca5e6
101 <tmddnjs001133@dsm.hs.kr>
2023-12-11 10:43:50 +0900

    프로필 공유

---
+++ src/main/java/com/example/instagramclonecoding/domain/share/service/shareProfile/ShareProfile.java
@@ -0,0 +1,24 @@
+package com.example.instagramclonecoding.domain.share.service.shareProfile;
+
+import com.example.instagramclonecoding.domain.share.service.ShareService;
+import com.example.instagramclonecoding.domain.user.service.facade.UserFacade;
+import org.springframework.stereotype.Service;
+import reactor.core.publisher.Mono;
+
+import java.awt.image.BufferedImage;
+
+@Service
+public class ShareProfile {
+    private final ShareService shareService;
+    private final UserFacade userFacade;
+
+    public ShareProfile(ShareService shareService, UserFacade userFacade) {
+        this.shareService = shareService;
+        this.userFacade = userFacade;
+    }
+
+    public Mono<BufferedImage> execute() {
+        return shareService.execute("user/" + userFacade.getUser().getId());
+    }
+
+}
