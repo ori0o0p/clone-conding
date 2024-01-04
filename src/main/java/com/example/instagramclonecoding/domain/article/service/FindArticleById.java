0e6a25b899f0e1c5081db2b2b2d0cc30de53e47d
101 <tmddnjs001133@dsm.hs.kr>
2024-01-04 22:11:27 +0900

    필요없는 코드 삭제

--- src/main/java/com/example/instagramclonecoding/domain/article/service/FindArticleById.java
+++ src/main/java/com/example/instagramclonecoding/domain/article/service/FindArticleById.java
@@ -1,18 +1,10 @@
 package com.example.instagramclonecoding.domain.article.service;
 
 import com.example.instagramclonecoding.domain.article.dto.ArticleResponse;
-import com.example.instagramclonecoding.domain.article.entity.Article;
-import com.example.instagramclonecoding.domain.article.repository.ArticleRepository;
 import com.example.instagramclonecoding.domain.article.service.facade.ArticleFacade;
 import com.example.instagramclonecoding.domain.article.service.facade.ArticleToArticleResponse;
-import com.example.instagramclonecoding.domain.comment.collection.CommentCollection;
-import com.example.instagramclonecoding.domain.like.collection.LikeCollection;
-import com.example.instagramclonecoding.domain.like.dto.LikeResponse;
-import com.example.instagramclonecoding.domain.comment.dto.CommentResponse;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Mono;
-
-import java.util.List;
 
 @Service
 public class FindArticleById {
--- src/main/java/com/example/instagramclonecoding/domain/user/service/FindArticlesByUser.java
+++ src/main/java/com/example/instagramclonecoding/domain/user/service/FindArticlesByUser.java
@@ -1,20 +1,12 @@
 package com.example.instagramclonecoding.domain.user.service;
 
 import com.example.instagramclonecoding.domain.article.dto.ArticleResponse;
-import com.example.instagramclonecoding.domain.article.entity.Article;
 import com.example.instagramclonecoding.domain.article.repository.ArticleRepository;
 import com.example.instagramclonecoding.domain.article.service.facade.ArticleToArticleResponse;
-import com.example.instagramclonecoding.domain.comment.collection.CommentCollection;
-import com.example.instagramclonecoding.domain.comment.dto.CommentResponse;
-import com.example.instagramclonecoding.domain.like.collection.LikeCollection;
-import com.example.instagramclonecoding.domain.like.dto.LikeResponse;
 import org.bson.types.ObjectId;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Flux;
-import reactor.core.publisher.Mono;
 import reactor.core.scheduler.Schedulers;
-
-import java.util.List;
 
 @Service
 public class FindArticlesByUser {
